5c1c3787a2c1e093779f2b80ae580237f7514d81
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2023-12-27 11:09:34 +0200

    wednesday in the office

--- src/studentcher-shared-utils/services/UsersService.ts
+++ src/studentcher-shared-utils/services/UsersService.ts
@@ -148,7 +148,7 @@ export class UsersService implements IUsersService {
   async getPersonalZone(data: IClientRequestData): Promise<ServiceResponse> {
     try {
       let privateZoneData: IUserPrivateZone = await this.userRepository.getPrivateZone(data);  // get all the data
-      privateZoneData.allActivities.reverse()
+      privateZoneData.allActivities.sort((a,b)=> a.index - b.index)                            // order the activities 1,2,3...
       privateZoneData = await this.userRepository.fixTheCurrentActivity(privateZoneData)     // get the real current activity
       let currentActivityIndex = privateZoneData.currentActivity.index -1
       let _ = privateZoneData.allActivities.find((activity)=> activity.index == privateZoneData.currentActivity.index  )
